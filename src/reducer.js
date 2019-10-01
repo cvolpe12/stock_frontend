ed32fc54d02b4eab9ce69e20ff19c57e682b6771
cvolpe12 <christopher.volpe12@gmail.com>
2019-10-01 14:40:19 -0400

    introduced Redux

---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+  allUserStocks = []
+}
+
+function reducer(state = initialState, action){
+  switch (action.type) {
+    case "GET_STOCKS":
+      return {...state, allUserStocks: action.payload}
+    default:
+      return state
+  }
+}
+
+export default reducer;
